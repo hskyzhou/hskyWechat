5e328fc9993d42e25b2cf565845e0487417ef285
hsky <xezw211@126.com>
2016-08-28 23:14:31 +0800

    wechat

--- app/Http/Controllers/Project/WechatController.php
+++ app/Http/Controllers/Project/WechatController.php
@@ -12,15 +12,18 @@ use EasyWeChat;
 class WechatController extends Controller{
     
     public function server(){
+		\Log::info('asfasf');
     	$server = EasyWeChat::server();
 
     	$server->setMessageHandler(function($message){
+
+    		\Log::info($message);
     		switch ($message->MsgType) {
 	            case 'subscribe':
 	                return '欢迎订阅';
 	                break;
 	            case 'text':
-	            	return '日期';
+	            	
 	            	break;
 	            default:
 	                # code...
