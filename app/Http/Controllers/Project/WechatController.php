238a1a508b9ad613d2f42c7d75d52e69cfea6dfb
hsky <xezw211@126.com>
2016-08-28 23:49:14 +0800

    wechat

--- app/Http/Controllers/Project/WechatController.php
+++ app/Http/Controllers/Project/WechatController.php
@@ -22,7 +22,7 @@ class WechatController extends Controller{
 	                break;
 	            case 'text':
 	            	$content = $message->Content;
-	            	$arr = explode($content);
+	            	$arr = explode(' ', $content);
 	            	$controller = 'App\\Controllers\\Project\\' . ucfirst(array_shift($arr)) . 'Controller';
 	            	\Log::info($controller);
 	            	return app()->call([$controller, 'index'], $arr);
